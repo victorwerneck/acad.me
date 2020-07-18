4fb300530b922225983b8ce755431ab4de450540
Gabriel Coelho <gaaucocoelho@gmail.com>
2020-07-17 22:18:46 -0300

    refactor: remove debug banner
    - set SignIn activity as the default/first one

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:acad_me/home.dart';
+import 'package:acad_me/sign_in.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Home(),));
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignIn(),
+    ));
